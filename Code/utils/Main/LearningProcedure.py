e311be137c050a319e451bc1e9432be7dc1a6558
simondn <simonnguyen1999@gmail.com>
2024-11-11 14:18:03 -0800

    One iteration function started working well

---
+++ Code/utils/Main/LearningProcedure.py
@@ -0,0 +1,47 @@
+### Import functions ###
+from utils.Main import *
+from utils.Selector import *
+from utils.Prediction import *
+
+def LearningProcedure(df_Train, 
+                         df_Test, 
+                         df_Candidate, 
+                         SelectorType, 
+                         SelectorArgs,
+                         ModelType, 
+                         ModelArgs):
+
+    ### Set Up ###
+    ErrorVec = []
+    SelectedObservationHistory = []
+
+    ### Algorithm
+    for i in range(0, len(df_Candidate)):
+
+        ### Prediction Model ###
+        Model = ModelType(**ModelArgs)
+        if "Model" in SelectorArgs.keys(): SelectorArgs['Model'] = Model            # NOTE: THIS IS NOT DYNAMIC
+
+        CurrentError = TestErrorFunction(Model, df_Test)
+        ErrorVec.append(CurrentError)
+
+        ### Sampling Procedure ###
+        QueryObservationIndex = SelectorType(**SelectorArgs)
+        QueryObservation = df_Candidate.loc[[QueryObservationIndex]] # or should this be iloc
+        SelectedObservationHistory.append(QueryObservationIndex)
+
+        # print("Iteration: ", i, "| QueryIndex: ", QueryObservationIndex, "| Inclusion: ", QueryObservationIndex in df_Candidate.index)
+        # print(df_Train)
+        # print(df_Candidate)
+        # print("---")
+        
+        ### Update Train and Candidate Sets ###
+        df_Train = pd.concat([df_Train, QueryObservation])
+        df_Candidate = df_Candidate.drop(QueryObservationIndex)
+
+        ### Update SelectorArgs and ModelArgs ###                                     # NOTE: THIS IS NOT DYNAMIC
+        if "df_Train" in ModelArgs.keys(): ModelArgs['df_Train'] = df_Train
+        if "df_Train" in SelectorArgs.keys(): SelectorArgs['df_Train'] = df_Train
+        if "df_Candidate" in SelectorArgs.keys(): SelectorArgs['df_Candidate'] = df_Candidate            
+
+    return ErrorVec, SelectedObservationHistory
