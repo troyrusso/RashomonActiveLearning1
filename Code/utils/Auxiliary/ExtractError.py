13e5fe448ab2af2864adc9cc69cb7d19d71aface
simondn <simonnguyen1999@gmail.com>
2024-11-14 23:06:07 -0800

    Fixed extract function

--- Code/utils/Auxiliary/ExtractError.py
+++ Code/utils/Auxiliary/ExtractError.py
@@ -3,20 +3,28 @@ import os
 import re
 import pickle
 import argparse
+import numpy as np
 import pandas as pd
 print("Chunk 1 good!")
 
-### Parser ###
-parser = argparse.ArgumentParser(description="Parse command line arguments for job parameters")
-# parser.add_argument("--ResultsDirectory", type=str, default="-1", help="Results Directory.")
-parser.add_argument("--DataType", type=str, default="-1", help="DataType.")
-parser.add_argument("--SelectorType", type=str, default="-1", help="SelectorType.")
-parser.add_argument("--ModelType", type=str, default="-1", help="ModelType.")
-args = parser.parse_args()
-print("Chunk 2 good!")
+# Manually define parser
+parser = argparse.ArgumentParser(description="Your script description here")
+parser.add_argument("--DataType", type=int, help="An example integer argument")
+parser.add_argument("--SelectorType", type=str, help="An example string argument")
+parser.add_argument("--ModelType", type=str, help="An example string argument")
+
+
+# Manually set args without parsing command-line input
+args = argparse.Namespace(DataType = "Simulate", 
+                          SelectorType = ["PassiveLearning", "GSx", "GSy", "iGS"],
+                          ModelType = "LinearRegression")
+
 
 ### Set Up  ###
 cwd = os.getcwd()
+# cwd = "/Users/simondn/Documents/RashomonActiveLearning/"
+
+
 ResultsDirectory = os.path.join(cwd, "Results")
 SaveDirectory = os.path.join(ResultsDirectory, args.DataType, "ProcessedResults")
 print("Chunk 3 good!")
@@ -30,38 +38,41 @@ AllSelectorMethodErrors = {}
 print("Chunk 5 good!")
 
 ### Iterate over directory files ###
-for FileName in os.listdir(Directory):
-    if FileName.endswith(".pkl"):
+AllSelectorMethodErrors = {selector: [] for selector in args.SelectorType}
 
-        ### Extract ###
-        match = re.match(r"Seed\d+_Data(\w+)_TP[\d.]+_CP[\d.]+_ST(\w+)_MT(\w+).pkl", FileName)
-        
-        ### Check Match ###
-        if match:
-            file_DataType = match.group(1)
-            file_SelectorType = match.group(2)
-            file_ModelType = match.group(3)
+for SelectorTypeIteration in args.SelectorType:
+    for FileName in os.listdir(Directory):
+        if FileName.endswith(".pkl"):
+            
+            ### Extract ###
+            match = re.match(r"Seed\d+_Data(\w+)_TP[\d.]+_CP[\d.]+_ST(\w+)_MT(\w+).pkl", FileName)
             
             ### Check Match ###
-            if file_DataType == args.DataType and file_SelectorType == args.SelectorType and file_ModelType == args.ModelType:
-
-                ### Load ###
-                with open(os.path.join(Directory, FileName), "rb") as f:
-                    SimulationResults = pickle.load(f)
+            if match:
+                file_DataType = match.group(1)
+                file_SelectorType = match.group(2)
+                file_ModelType = match.group(3)
                 
-                ### Extract the ErrorVec ###
-                ErrorVector = SimulationResults["ErrorVec"]
+                ### Check Match ###
+                MatchCheck = (file_DataType == args.DataType and file_SelectorType == SelectorTypeIteration and file_ModelType == args.ModelType)
+                if MatchCheck:
+                    ### Load ###
+                    with open(os.path.join(Directory, FileName), "rb") as f:
+                        SimulationResults = pickle.load(f)
+                    
+                    ### Extract the ErrorVec ###
+                    ErrorVector = SimulationResults["ErrorVec"]
 
-                ### Append ###
-                AllSelectorMethodErrors[file_SelectorType] = pd.concat([AllSelectorMethodErrors[file_SelectorType], ErrorVector], 
-                                                                        ignore_index=True)
-print("Chunk 6 good!")
+                    ### Append ### 
+                    AllSelectorMethodErrors[SelectorTypeIteration].append(ErrorVector)
+    AllSelectorMethodErrors[SelectorTypeIteration] = pd.DataFrame(np.array(AllSelectorMethodErrors[SelectorTypeIteration]).squeeze())
+    print("Chunk 6 good!")
 
 #### Save to CSV ###
 for file_SelectorType, df in AllSelectorMethodErrors.items():
     print("preparing to save this shit")
     csv_file = os.path.join(SaveDirectory, f"{file_SelectorType}_ErrorVec.csv")
-    df.to_csv(csv_file, index=False)
+    df.to_csv(csv_file, index=False, header= False)
     print(f"Saved {file_SelectorType}_ErrorVec.csv")
     print("did this shit save")
 print("Chunk 7 good!")
