9fec1d933d6ed009c285a0412e12086284bef558
simondn <simonnguyen1999@gmail.com>
2024-11-15 01:43:50 -0800

    Edit git ignore

--- Code/utils/Auxiliary/MeanVariancePlot.py
+++ Code/utils/Auxiliary/MeanVariancePlot.py
@@ -4,27 +4,40 @@ import matplotlib.pyplot as plt
 
 ### Function ###
 def MeanVariancePlot(Subtitle = None,
+                     TransparencyVal = 0.2,
+                     CriticalValue = 1.96,
                      **SimulationErrorResults):
 
     ### Set Up ###
     MeanVector = {}
     VarianceVector = {}
+    StdErrorVector ={}
 
     ### Extract ###
     for Label, Results in SimulationErrorResults.items():
         MeanVector[Label] = np.mean(Results, axis =0)
         VarianceVector[Label] = np.var(Results, axis =0)
+        StdErrorVector[Label] = np.std(Results, axis=0) / np.sqrt(Results.shape[0])
+
 
     ### Mean Plot ###
     plt.figure(figsize=(7, 6))
     for Label, MeanValues in MeanVector.items():
-        plt.plot(range(len(MeanValues)), MeanValues, label=Label)
+        StdErrorValues = StdErrorVector[Label]
+        x = range(len(MeanValues))
+        plt.plot(x, MeanValues, label=Label)
+        plt.fill_between(x, MeanValues - CriticalValue * StdErrorValues, 
+                        MeanValues + CriticalValue * StdErrorValues, alpha=TransparencyVal)
+
     plt.suptitle("Active Learning Mean RMSE Plot")
     plt.xlabel("Number of labelled observations")
     plt.ylabel("Mean RMSE")
-    plt.title(Subtitle, fontsize = 9)
+    plt.title(Subtitle, fontsize=9)
     plt.legend()
     MeanPlot = plt.gcf()
+
+
+    ####
     
     ### Variance Plot ###
     plt.figure(figsize=(7, 6))
