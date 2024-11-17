5af6e704ff2f8ac3bc244655479c7b3f89d0fafb
simondn <simonnguyen1999@gmail.com>
2024-11-17 15:52:24 -0800

    Added Creationfiles to ignore __init__

--- Code/utils/Auxiliary/__init__.py
+++ Code/utils/Auxiliary/__init__.py
@@ -7,7 +7,7 @@ current_dir = os.path.dirname(__file__)
 # Loop through all files in the directory and import them dynamically
 for filename in os.listdir(current_dir):
     # Only import .py files (and skip __init__.py and __utils__.py)
-    if filename.endswith(".py") and filename not in ["__init__.py", "__utils__.py", "ExtractError.py"]:
+    if filename.endswith(".py") and filename not in ["__init__.py", "__utils__.py", "ExtractError.py", "CreateRunSimSbatch.py", "CreateExtractSbatch.py"]:
         module_name = filename[:-3]  # Remove the ".py" extension
         module = importlib.import_module(f".{module_name}", package=__name__)  # Import the module
         
