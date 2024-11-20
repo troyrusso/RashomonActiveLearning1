7b7ebb132407360c2390699d4f49a7d4938df614
simondn <simonnguyen1999@gmail.com>
2024-11-19 17:16:12 -0800

    Make LoadData generalizeable

--- Code/utils/Auxiliary/LoadDataSet.py
+++ Code/utils/Auxiliary/LoadDataSet.py
@@ -12,8 +12,7 @@ def LoadData(filename):
     cwd = os.getcwd()
     ParentDirectory = os.path.abspath(os.path.join(cwd, "../"))
     # CurrentDirectory = ParentDirectory                              # NOTE: FOR LOCAL SIMULATIONS
-    # CurrentDirectory = cwd                                        # NOTE: FOR THE CLUSTER
-    CurrentDirectory = "~/RashomonActiveLearning/"
+    CurrentDirectory = cwd                                        # NOTE: FOR THE CLUSTER
 
     ### File Path ###
     filepath = os.path.join(CurrentDirectory, "Data","processed", filename +".pkl")
