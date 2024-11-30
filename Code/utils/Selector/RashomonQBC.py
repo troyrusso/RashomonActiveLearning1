6cecc268f8ff89a3f8f122affa7b78ac79520953
simondn <simonnguyen1999@gmail.com>
2024-11-30 13:48:27 -0800

    VoteEntropy

--- Code/utils/Selector/RashomonQBC.py
+++ Code/utils/Selector/RashomonQBC.py
@@ -16,23 +16,39 @@ from scipy.spatial.distance import cdist
 ### Function ###
 def RashomonQBCFunction(Model, df_Candidate, df_Train, TopCModels, AllErrors):
 
-    # ### GSx ### Incorporate? Good for tie breakers.
+    # ### GSx: Incorporate? Good for tie breakers. Could also do GSx on the data set as opposed to the one-hot set
     # d_nmX = cdist(df_Candidate.loc[:,df_Candidate.columns!= "Y"], df_Train.loc[:,df_Train.columns!= "Y"], metric = distance)
     # d_nX = d_nmX.min(axis=1)
 
-    ### Extract Errors ###
+    ### Errors and Prediction###
     if len(AllErrors) < TopCModels:
         TopCModels = len(AllErrors)
     LowestErrorIndices = np.argsort(AllErrors)[:TopCModels]
-
-    ### Prediction ###
     PredictedValues = [Model[i].predict(df_Candidate) for i in LowestErrorIndices]
-    RashomonMean = np.array(PredictedValues).mean(axis =0)
+    PredictedValues = np.vstack(PredictedValues)
+
+
+    ### Vote Entropy ###
+    VoteC = {}
+    LogVoteC = {}
+    VoteEntropy = {}
+    UniqueClasses = set(df_Train["Y"])
+
+    # Vote entropy per class #
+    for classes in UniqueClasses:
+        VoteC[classes] = np.mean(PredictedValues == classes, axis=0)
+        LogVoteC[classes] = np.log(VoteC[classes])
+        VoteEntropy[classes] =  - VoteC[classes] * LogVoteC[classes]
+        VoteEntropy[classes] = np.nan_to_num(VoteEntropy[0], nan=0)
+
+    # Vote Entropy #
+    VoteEntropyMatrix = np.stack(list(VoteEntropy.values()), axis=1)
+    VoteEntropyFinal = np.sum(VoteEntropyMatrix, axis=1)
 
     ### Uncertainty Metric ###
-    df_Candidate["UncertaintyMetric"] = abs(RashomonMean - 0.5)
-    IndexRecommendation = int(df_Candidate.sort_values(by = "UncertaintyMetric", ascending = True).index[0])
-    df_Candidate.drop('UncertaintyMetric', axis=1, inplace=True)
+    df_Candidate["UncertaintyMetric"] = VoteEntropyFinal
+    IndexRecommendation = int(df_Candidate.sort_values(by = "UncertaintyMetric", ascending = False).index[0])
+    # df_Candidate.drop('UncertaintyMetric', axis=1, inplace=True)
     
     return(IndexRecommendation)
 
