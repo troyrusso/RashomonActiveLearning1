cb9cb42a3f30219bac0f95f07c2ec8320f2b8e7f
simondn <simonnguyen1999@gmail.com>
2024-12-04 01:21:29 -0800

    UniqueError TreeFarms works in OneIteration

--- Code/utils/Selector/TreeEnsembleQBCFunction.py
+++ Code/utils/Selector/TreeEnsembleQBCFunction.py
@@ -15,7 +15,7 @@ import numpy as np
 from scipy.spatial.distance import cdist
 
 ### Function ###
-def TreeEnsembleQBCFunction(Model, df_Candidate, df_Train, TopCModels, AllErrors):
+def TreeEnsembleQBCFunction(Model, df_Candidate, df_Train, TopCModels, AllErrors, UniqueErrorsInput):
 
     ### Ignore warning (taken care of) ###
     np.seterr(all = 'ignore') 
@@ -30,10 +30,23 @@ def TreeEnsembleQBCFunction(Model, df_Candidate, df_Train, TopCModels, AllErrors
     if 'TREEFARMS' in str(type(Model)):                                                                         # TreeFarms
         if len(AllErrors) < TopCModels:
             TopCModels = len(AllErrors)
-        LowestErrorIndices = np.argsort(AllErrors)[:TopCModels]
-        PredictedValues = [Model[i].predict(df_Candidate) for i in LowestErrorIndices]                          # RandomForest
-    elif 'RandomForestClassifier' in str(type(Model)):
+
+        ## Unique Errors ##
+        if UniqueErrorsInput:
+            AllErrorsArray = np.array(AllErrors)
+            UniqueErrors = sorted(set(AllErrors))
+            SelectedErrors = UniqueErrors[:TopCModels]
+            LowestErrorIndices = [int(np.where(AllErrorsArray == error)[0][0]) for error in SelectedErrors]
+        else:
+            LowestErrorIndices = np.argsort(AllErrors)[:TopCModels]
+
+        ## Prediction ##
+        PredictedValues = [Model[i].predict(df_Candidate) for i in LowestErrorIndices]
+
+    elif 'RandomForestClassifier' in str(type(Model)):                                                          # RandomForest
         PredictedValues = [Model.estimators_[tree].predict(df_Candidate.loc[:, df_Candidate.columns != "Y"]) for tree in range(TopCModels)] 
+    
+    ### Stack values ###
     PredictedValues = np.vstack(PredictedValues)
 
     ### Vote Entropy ###
@@ -47,7 +60,7 @@ def TreeEnsembleQBCFunction(Model, df_Candidate, df_Train, TopCModels, AllErrors
         VoteC[classes] = np.mean(PredictedValues == classes, axis=0)
         LogVoteC[classes] = np.log(VoteC[classes])
         VoteEntropy[classes] =  - VoteC[classes] * LogVoteC[classes]
-        VoteEntropy[classes] = np.nan_to_num(VoteEntropy[0], nan=0)
+        VoteEntropy[classes] = np.nan_to_num(VoteEntropy[classes], nan=0)
 
     # Vote Entropy #
     VoteEntropyMatrix = np.stack(list(VoteEntropy.values()), axis=1)
