5a3e1bc81b2ad4d11c0eeb621a846b8816e65d29
simondn <simonnguyen1999@gmail.com>
2024-12-18 01:48:38 -0800

    Create DeleteSimulationFile

--- Code/Cluster/BostonHousing/DeleteSimulationFiles.sh
+++ Code/Cluster/BostonHousing/DeleteSimulationFiles.sh
@@ -1,32 +1,20 @@
 #!/bin/bash
 
-# Navigate to the RunSimulations directory
+### Navigate to RunSimulations Directory ###
 cd RunSimulations
 
-# Run delete_sbatch.sh
-if [[ -f delete_sbatch.sh ]]; then
-    echo "Running delete_sbatch.sh..."
-    bash delete_sbatch.sh
-else
-    echo "delete_sbatch.sh not found in RunSimulations."
-fi
+### Delete all .sbatch files ###
+bash delete_sbatch.sh
+echo "All .sbatch files deleted."
 
-# Navigate to ClusterMessages/out and run delete_out.sh
+### Delete all .out files ###
 cd ClusterMessages/out
-if [[ -f delete_out.sh ]]; then
-    echo "Running delete_out.sh..."
-    bash delete_out.sh
-else
-    echo "delete_out.sh not found in ClusterMessages/out."
-fi
+bash delete_out.sh
+echo "All .out files deleted."
 
-# Navigate to ClusterMessages/error and run delete_error.sh
+### Delete all .err files ###
 cd ../error
-if [[ -f delete_error.sh ]]; then
-    echo "Running delete_error.sh..."
-    bash delete_error.sh
-else
-    echo "delete_error.sh not found in ClusterMessages/error."
-fi
+bash delete_error.sh
+echo "All .error files deleted."
 
-echo "All delete scripts executed."
+### Delete all Unprocessed Results files ##
