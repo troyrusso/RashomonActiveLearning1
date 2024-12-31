c9799dc17b58ef3cd016c1c6f0fa754225a1ed31
simondn <simonnguyen1999@gmail.com>
2024-12-31 13:04:28 -0800

    Process Sim works

--- Code/Cluster/BostonHousing/4_DeleteSimulationFiles.sh
+++ Code/Cluster/BostonHousing/4_DeleteSimulationFiles.sh
@@ -9,17 +9,14 @@ cd RunSimulations
 
 ### Delete all .sbatch files ###
 bash delete_sbatch.sh
-echo "All .sbatch files deleted."
 
 ### Delete all .out files ###
 cd ClusterMessages/out
 bash delete_out.sh
-echo "All .out files deleted."
 
 ### Delete all .err files ###
 cd ../error
 bash delete_error.sh
-echo "All .error files deleted."
 
 ### Delete all Unprocessed Results files ##
 # Remove Random Forest Results #
