89ce5dd4f18e264373c3ce2870b4155c3a938b14
simondn <simonnguyen1999@gmail.com>
2024-12-31 17:47:38 -0800

    Sending 0.025 simulation

--- Code/utils/Auxiliary/LoadAnalyzedData.py
+++ Code/utils/Auxiliary/LoadAnalyzedData.py
@@ -4,23 +4,31 @@ import os
 import pandas as pd
 
 ### Function ###
-def LoadAnalyzedData(data_type, base_directory, parameter):
+def LoadAnalyzedData(data_type, base_directory, method, parameter):
 
-    ResultsDirectory = os.path.join(base_directory, data_type)
+    ResultsDirectory = os.path.join(base_directory, data_type, method)
 
     ### File Path ###
-    PathTemplates = {
-        "Error_UNREAL": f"TreeFarms/ProcessedResults/ErrorVec/DPL{parameter}_ErrorMatrix.csv",
-        "Error_DUREAL": f"TreeFarms/ProcessedResults/ErrorVec/UNQ{parameter}_ErrorMatrix.csv",
-        "Time_UNREAL": f"TreeFarms/ProcessedResults/ElapsedTime/UNQ{parameter}_TimeMatrix.csv",
-        "Time_DUREAL": f"TreeFarms/ProcessedResults/ElapsedTime/DPL{parameter}_TimeMatrix.csv",
-        "SelectionHistory_UNREAL": f"TreeFarms/ProcessedResults/SelectionHistory/UNQ{parameter}_SelectionHistory.csv",
-        "SelectionHistory_DUREAL": f"TreeFarms/ProcessedResults/SelectionHistory/DPL{parameter}_SelectionHistory.csv",
-        "TreeCounts_UNREAL": f"TreeFarms/ProcessedResults/ElapsedTime/UNQ{parameter}_TimeMatrix.csv",
-        "TreeCounts_DUREAL": f"TreeFarms/ProcessedResults/ElapsedTime/DPL{parameter}_TimeMatrix.csv",
-        "TreeCounts_ALL_UNREAL": f"TreeFarms/ProcessedResults/TreeCount/UNQ{parameter}_AllTreeCount.csv",
-        "TreeCounts_UNQ_UNREAL": f"TreeFarms/ProcessedResults/TreeCount/UNQ{parameter}_UniqueTreeCount.csv",
-    }
+
+    if method == "RandomForestClassification":
+        PathTemplates = {
+            "Error_RF": f"ProcessedResults/ErrorVec/RF{parameter}_ErrorMatrix.csv",
+            "Time_RF": f"ProcessedResults/ElapsedTime/RF{parameter}_TimeMatrix.csv",
+            "SelectionHistory_RF": f"ProcessedResults/SelectionHistory/RF{parameter}_SelectionHistory.csv"
+        }
+    if method == "TreeFarms":
+        PathTemplates = {
+            "Error_UNREAL": f"ProcessedResults/ErrorVec/DPL{parameter}_ErrorMatrix.csv",
+            "Error_DUREAL": f"ProcessedResults/ErrorVec/UNQ{parameter}_ErrorMatrix.csv",
+            "Time_UNREAL": f"ProcessedResults/ElapsedTime/UNQ{parameter}_TimeMatrix.csv",
+            "Time_DUREAL": f"ProcessedResults/ElapsedTime/DPL{parameter}_TimeMatrix.csv",
+            "SelectionHistory_UNREAL": f"ProcessedResults/SelectionHistory/UNQ{parameter}_SelectionHistory.csv",
+            "SelectionHistory_DUREAL": f"ProcessedResults/SelectionHistory/DPL{parameter}_SelectionHistory.csv",
+            "TreeCounts_UNREAL": f"ProcessedResults/ElapsedTime/UNQ{parameter}_TimeMatrix.csv",
+            "TreeCounts_DUREAL": f"ProcessedResults/ElapsedTime/DPL{parameter}_TimeMatrix.csv",
+            "TreeCounts_ALL_UNREAL": f"ProcessedResults/TreeCount/UNQ{parameter}_AllTreeCount.csv",
+            "TreeCounts_UNQ_UNREAL": f"ProcessedResults/TreeCount/UNQ{parameter}_UniqueTreeCount.csv",
+        }
 
     #### Load Data Into Dictionary ###
     DataDictiomary = {}
