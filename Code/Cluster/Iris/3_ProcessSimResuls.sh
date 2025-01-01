256570fab1abd0ce59c4a434802f897a08149247
simondn <simonnguyen1999@gmail.com>
2025-01-01 15:55:33 -0800

    Sending Iris 0.015 simulations

---
+++ Code/Cluster/Iris/3_ProcessSimResuls.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+### Current Directory Name ###
+CURRENT_DIR=$(basename "$PWD")
+echo "Processing results for dataset: $CURRENT_DIR"
+
+### Extract Random Forests Results ###
+cd ~/RashomonActiveLearning
+python Code/utils/Auxiliary/ProcessSimulationResults.py \
+    --DataType "$CURRENT_DIR" \
+    --ModelType "RandomForestClassification" \
+    --Categories "RF0.01.pkl"
+
+### Extract Duplicate TREEFARMS Results ###
+python Code/utils/Auxiliary/ProcessSimulationResults.py \
+    --DataType "$CURRENT_DIR" \
+    --ModelType "TreeFarms" \
+    --Categories "DPL0.025.pkl"
+
+### Extract Unique TREEFARMS Results ###
+python Code/utils/Auxiliary/ProcessSimulationResults.py \
+    --DataType "$CURRENT_DIR" \
+    --ModelType "TreeFarms" \
+    --Categories "UNQ0.025.pkl"
