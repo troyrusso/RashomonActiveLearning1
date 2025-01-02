f674ccb5349d79712071eec80e52eb13fb0f457f
Simon Dovan Nguyen <simondn@cls-lg.stat.washington.edu>
2025-01-02 15:21:23 -0800

    IRIS and CPS results

--- Code/Cluster/BostonHousing/3_ProcessSimResuls.sh
+++ Code/Cluster/BostonHousing/3_ProcessSimResuls.sh
@@ -15,10 +15,10 @@ python Code/utils/Auxiliary/ProcessSimulationResults.py \
 python Code/utils/Auxiliary/ProcessSimulationResults.py \
     --DataType "$CURRENT_DIR" \
     --ModelType "TreeFarms" \
-    --Categories "DPL0.025.pkl"
+    --Categories "DPL0.015.pkl"
 
 ### Extract Unique TREEFARMS Results ###
 python Code/utils/Auxiliary/ProcessSimulationResults.py \
     --DataType "$CURRENT_DIR" \
     --ModelType "TreeFarms" \
-    --Categories "UNQ0.025.pkl"
+    --Categories "UNQ0.015.pkl"
