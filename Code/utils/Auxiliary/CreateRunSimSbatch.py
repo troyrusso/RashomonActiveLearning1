95fdd05916feae8ab5b93b1c249b57d10b043dee
simondn <simonnguyen1999@gmail.com>
2025-01-06 15:39:11 -0800

    Sending F1 0.015 simulations

--- Code/utils/Auxiliary/CreateRunSimSbatch.py
+++ Code/utils/Auxiliary/CreateRunSimSbatch.py
@@ -50,7 +50,7 @@ for i, row in ParameterVector.iterrows():
         "#SBATCH --mem-per-cpu=30000",
         f"#SBATCH -o ClusterMessages/out/myscript_{JobName}_%j.out",
         f"#SBATCH -e ClusterMessages/error/myscript_{JobName}_%j.err",
-        "#SBATCH --mail-type=ALL",
+        "#SBATCH --mail-type=FAIL",                                             # FAIL ALL
         "#SBATCH --mail-user=simondn@uw.edu",
         "",
         "cd ~/RashomonActiveLearning",
