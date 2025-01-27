791a48075c996e8c611676cdb8a5484af4fd304d
simondn <simonnguyen1999@gmail.com>
2025-01-27 13:35:12 -0800

    Preparing sbatch optimal values

---
+++ Code/Cluster/OptimalValues/Iris/0IS_FRT.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=FindThreshold_BC
+#SBATCH --partition=compute
+#SBATCH --ntasks=1
+#SBATCH --time=23:59:00
+#SBATCH --mem-per-cpu=30G
+#SBATCH -o ClusterMessages/out/FindThreshold_BC.06_%j.out
+#SBATCH -e ClusterMessages/error/FindThreshold_BC.06_%j.err
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=simondn@uw.edu
+
+cd ~/RashomonActiveLearning
+module load Python
+python Code/OptimalThresholdSimulation.py \
+    --JobName OptimalThresholdTest \
+    --Data MONK3
+     \
+    --Seed 0 \
+    --TestProportion 0.2 \
+    --CandidateProportion 0.8 \
+    --regularization 0.01 \
+    --RashomonThresholdType Adder \
+    --RashomonThreshold 0.1 \
+    --Output TEST.pkl
+
+
+
