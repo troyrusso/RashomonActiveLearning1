adfc84326c655ecd7bad639be76817df9d33f916
simondn <simonnguyen1999@gmail.com>
2025-01-27 23:50:14 -0800

    Preparing FRT for new 4

---
+++ Code/Cluster/OptimalValues/Bar7/4_DeleteFRTFiles.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+### Get the current directory name ###
+CURRENT_DIR=$(basename "$PWD")
+echo "Current directory is: $CURRENT_DIR"
+
+### Navigate to RunSimulations Directory ###
+cd RunFRT
+
+### Delete all .sbatch files ###
+bash delete_sbatch.sh
+
+### Delete all .out files ###
+cd ClusterMessages/out
+bash delete_out.sh
+
+### Delete all .err files ###
+cd ../error
+bash delete_error.sh
+
+### Delete all Unprocessed Results files ##
+# Remove Random Forest Results #
+cd ../../../../../../Results/"OptimalThreshold"/"$CURRENT_DIR"/Raw
+bash delete_results.sh
+echo "All .pkl results files in FRT deleted."
+
