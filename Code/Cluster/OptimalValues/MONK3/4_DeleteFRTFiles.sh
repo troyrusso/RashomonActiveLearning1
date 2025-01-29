64dee105ac12083fa661bfda0f4a67c62e3090ee
simondn <simonnguyen1999@gmail.com>
2025-01-28 16:03:58 -0800

    FRT still not high resolution

--- Code/Cluster/OptimalValues/MONK3/4_DeleteFRTFiles.sh
+++ Code/Cluster/OptimalValues/MONK3/4_DeleteFRTFiles.sh
@@ -20,7 +20,7 @@ bash delete_error.sh
 
 ### Delete all Unprocessed Results files ##
 # Remove Random Forest Results #
-cd ../../../../../../Results/"OptimalThreshold"/"$CURRENT_DIR"/Raw
+cd ../../../../../../../Results/OptimalThreshold/"$CURRENT_DIR"/Raw
 bash delete_results.sh
 echo "All .pkl results files in FRT deleted."
 
