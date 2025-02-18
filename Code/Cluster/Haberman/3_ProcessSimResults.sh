55210e6c749ff07792b65ff8b62425ac62890357
simondn <simonnguyen1999@gmail.com>
2025-02-17 20:47:19 -0800

    Sending PL and RF BankNote and Haberman

---
+++ Code/Cluster/Haberman/3_ProcessSimResults.sh
@@ -0,0 +1,30 @@
+### Current Directory Name ###
+CURRENT_DIR=$(basename "$PWD")
+echo "Processing results for dataset: $CURRENT_DIR"
+cd ~/RashomonActiveLearning
+
+### Extract PassiveLearning Results ###
+cd ~/RashomonActiveLearning
+python Code/utils/Auxiliary/ProcessSimulationResults.py \
+    --DataType "$CURRENT_DIR" \
+    --ModelType "RandomForestClassification" \
+    --Categories "PLA0.pkl"
+
+### Extract Random Forests Results ###
+cd ~/RashomonActiveLearning
+python Code/utils/Auxiliary/ProcessSimulationResults.py \
+    --DataType "$CURRENT_DIR" \
+    --ModelType "RandomForestClassification" \
+    --Categories "RFA0.pkl"
+
+# ### Extract Duplicate TREEFARMS Results ###
+# python Code/utils/Auxiliary/ProcessSimulationResults.py \
+#     --DataType "$CURRENT_DIR" \
+#     --ModelType "TreeFarms" \
+#     --Categories "DA0.02.pkl"
+
+# ### Extract Unique TREEFARMS Results ###
+# python Code/utils/Auxiliary/ProcessSimulationResults.py \
+#     --DataType "$CURRENT_DIR" \
+#     --ModelType "TreeFarms" \
+#     --Categories "UA0.02.pkl"
--- Code/Cluster/OptimalValues/MONK3/5_DeleteFRTRawResults.sh
+++ Code/Cluster/OptimalValues/MONK3/5_DeleteFRTRawResults.sh
@@ -5,7 +5,7 @@ CURRENT_DIR=$(basename "$PWD")
 echo "Current directory is: $CURRENT_DIR"
 
 ### Delete all Unprocessed Results files ##
-cd ~RashomonActiveLearning/Results/OptimalThreshold/"$CURRENT_DIR"/Raw
+cd ../../../../Results/OptimalThreshold/"$CURRENT_DIR"/Raw
 bash delete_results.sh
 echo "All .pkl results files in FRT deleted."
 
