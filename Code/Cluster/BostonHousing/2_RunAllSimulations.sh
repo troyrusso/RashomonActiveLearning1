5701c0c0ccf8a6d613a3e009435fa5ca56e49c78
trusso96 <trusso96@uw.edu>
2025-04-22 07:59:31 -0700

    changed run sbatch

--- Code/Cluster/BostonHousing/2_RunAllSimulations.sh
+++ Code/Cluster/BostonHousing/2_RunAllSimulations.sh
@@ -1,5 +1,9 @@
-cd RunSimulations
-for file in ./*sbatch
-do
-	sbatch "$file"
+# cd RunSimulations
+# for file in ./*sbatch
+# do
+# 	sbatch "$file"
+# done
+#cd ~/RashomonActiveLearning1/Code/Cluster/BostonHousing
+for job in *.sbatch; do
+  sbatch -A stf -p compute "$job"
 done
