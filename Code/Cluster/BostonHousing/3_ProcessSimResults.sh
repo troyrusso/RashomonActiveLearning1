8484f1c835d603c93614e5ecfaa69542c0724164
trusso96 <trusso96@uw.edu>
2025-04-23 18:31:08 -0700

    new 3 entirely

--- Code/Cluster/BostonHousing/3_ProcessSimResults.sh
+++ Code/Cluster/BostonHousing/3_ProcessSimResults.sh
@@ -1,22 +1,42 @@
 ### Current Directory Name ###
+# CURRENT_DIR=$(basename "$PWD")
+# echo "Processing results for dataset: $CURRENT_DIR"
+# cd ~/RashomonActiveLearning1
+
+# ### Extract PassiveLearning Results ###
+# cd ~/RashomonActiveLearning1
+# python Code/utils/Auxiliary/ProcessSimulationResults.py \
+#     --DataType "$CURRENT_DIR" \
+#     --ModelType "RandomForestClassification" \
+#     --Categories "PLA0.pkl"
+
+# ### Extract Random Forests Results ###
+# cd ~/RashomonActiveLearning1
+# #RandomForestClassification
+# python Code/utils/Auxiliary/ProcessSimulationResults.py \
+#     --DataType "$CURRENT_DIR" \
+#     --ModelType "RandomForestClassification" \
+#     --Categories "RFA0.pkl"
+
+#chat gpt one here 
+#!/usr/bin/env bash
+cd "$(dirname "$0")"
+
 CURRENT_DIR=$(basename "$PWD")
 echo "Processing results for dataset: $CURRENT_DIR"
-cd ~/RashomonActiveLearning1
 
-### Extract PassiveLearning Results ###
-cd ~/RashomonActiveLearning1
+# Duplicate‐TreeFarms (ε=0.025)
 python Code/utils/Auxiliary/ProcessSimulationResults.py \
-    --DataType "$CURRENT_DIR" \
-    --ModelType "RandomForestClassification" \
-    --Categories "PLA0.pkl"
+  --DataType   "$CURRENT_DIR" \
+  --ModelType  "TreeFarms" \
+  --Categories "DPL0.025"
 
-### Extract Random Forests Results ###
-cd ~/RashomonActiveLearning1
-#RandomForestClassification
+# Unique‐TreeFarms (ε=0.025)
 python Code/utils/Auxiliary/ProcessSimulationResults.py \
-    --DataType "$CURRENT_DIR" \
-    --ModelType "RandomForestClassification" \
-    --Categories "RFA0.pkl"
+  --DataType   "$CURRENT_DIR" \
+  --ModelType  "TreeFarms" \
+  --Categories "UNQ0.025"
+
 
 # ### Extract Duplicate TREEFARMS Results ###
 # python Code/utils/Auxiliary/ProcessSimulationResults.py \
