adc7a443cba3d1703a7fdefb95ba94960e465c28
trusso96 <trusso96@uw.edu>
2025-04-25 16:01:07 -0700

    bakc to original 2

--- Code/Cluster/BostonHousing/2_RunAllSimulations.sh
+++ Code/Cluster/BostonHousing/2_RunAllSimulations.sh
@@ -1,18 +1,18 @@
-# cd RunSimulations
-# for file in ./*sbatch
-# do
-# 	sbatch "$file"
-# done
-
-#!/usr/bin/env bash
-# Go to the RunSimulations subdirectory
-cd "$(dirname "$0")/RunSimulations"
-
-# Don’t treat an empty glob as literal “*.sbatch”
-shopt -s nullglob
-
-# Loop and submit
-for job in *.sbatch; do
-  echo "Submitting $job → compute"
-  sbatch -A stf -p compute "$job"
+cd RunSimulations
+for file in ./*sbatch
+do
+	sbatch "$file"
 done
+
+##!/usr/bin/env bash
+# Go to the RunSimulations subdirectory
+# cd "$(dirname "$0")/RunSimulations"
+
+# # Don’t treat an empty glob as literal “*.sbatch”
+# shopt -s nullglob
+
+# # Loop and submit
+# for job in *.sbatch; do
+#   echo "Submitting $job → compute"
+#   sbatch -A stf -p compute "$job"
+# done
