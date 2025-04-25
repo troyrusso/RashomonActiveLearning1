52794b7c0c15b0f93840981182084570bb2f55a2
trusso96 <trusso96@uw.edu>
2025-04-25 10:13:22 -0700

    back to original 3

--- Code/Cluster/BostonHousing/3_ProcessSimResults.sh
+++ Code/Cluster/BostonHousing/3_ProcessSimResults.sh
@@ -1,45 +1,45 @@
 ### Current Directory Name ###
-# CURRENT_DIR=$(basename "$PWD")
-# echo "Processing results for dataset: $CURRENT_DIR"
-# cd ~/RashomonActiveLearning1
-
-# ### Extract PassiveLearning Results ###
-# cd ~/RashomonActiveLearning1
-# python Code/utils/Auxiliary/ProcessSimulationResults.py \
-#     --DataType "$CURRENT_DIR" \
-#     --ModelType "RandomForestClassification" \
-#     --Categories "PLA0.pkl"
-
-# ### Extract Random Forests Results ###
-# cd ~/RashomonActiveLearning1
-# #RandomForestClassification
-# python Code/utils/Auxiliary/ProcessSimulationResults.py \
-#     --DataType "$CURRENT_DIR" \
-#     --ModelType "RandomForestClassification" \
-#     --Categories "RFA0.pkl"
-
-#chat gpt one here 
-#!/usr/bin/env bash
-
-# 1) Jump to project root
+CURRENT_DIR=$(basename "$PWD")
+echo "Processing results for dataset: $CURRENT_DIR"
 cd ~/RashomonActiveLearning1
 
-DATASET=BostonHousing
-MODELT=TreeFarms
-
-echo "Processing results for dataset: $DATASET, model: $MODELT"
-
-# 2) Process DPL0.025
+### Extract PassiveLearning Results ###
+cd ~/RashomonActiveLearning1
 python Code/utils/Auxiliary/ProcessSimulationResults.py \
-  --DataType   "$DATASET" \
-  --ModelType  "$MODELT" \
-  --Categories "DPL0.025"
+    --DataType "$CURRENT_DIR" \
+    --ModelType "RandomForestClassification" \
+    --Categories "PLA0.pkl"
 
-# 3) Process UNQ0.025
+### Extract Random Forests Results ###
+cd ~/RashomonActiveLearning1
+#RandomForestClassification
 python Code/utils/Auxiliary/ProcessSimulationResults.py \
-  --DataType   "$DATASET" \
-  --ModelType  "$MODELT" \
-  --Categories "UNQ0.025"
+    --DataType "$CURRENT_DIR" \
+    --ModelType "RandomForestClassification" \
+    --Categories "RFA0.pkl"
+
+# #chat gpt one here 
+# #!/usr/bin/env bash
+
+# # 1) Jump to project root
+# cd ~/RashomonActiveLearning1
+
+# DATASET=BostonHousing
+# MODELT=TreeFarms
+
+# echo "Processing results for dataset: $DATASET, model: $MODELT"
+
+# # 2) Process DPL0.025
+# python Code/utils/Auxiliary/ProcessSimulationResults.py \
+#   --DataType   "$DATASET" \
+#   --ModelType  "$MODELT" \
+#   --Categories "DPL0.025"
+
+# # 3) Process UNQ0.025
+# python Code/utils/Auxiliary/ProcessSimulationResults.py \
+#   --DataType   "$DATASET" \
+#   --ModelType  "$MODELT" \
+#   --Categories "UNQ0.025"
 
 
 
